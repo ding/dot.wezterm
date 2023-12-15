5604d55d6a06705e46a6c07ff5399575d4a6f94a
yong.ding <yong.ding@ebaotech.com>
2023-12-15 20:32:42 +0800

    重构整个文件，追加各系统判断

--- wezterm.lua
+++ wezterm.lua
@@ -1,39 +1,63 @@
 -- 20230321 from https://github.com/ivaquero/oxidizer/blob/master/defaults/wezterm.lua
 -- 
-local wezterm = require("wezterm")
+local wezterm = require 'wezterm'
+local config = {}
+local haswork,work = pcall(require,"work")
 
 -- x86_64-pc-windows-msvc - Windows
 -- x86_64-apple-darwin - macOS (Intel)
 -- aarch64-apple-darwin - macOS (Apple Silicon)
 -- x86_64-unknown-linux-gnu - Linux
 
--- if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-if string.match(wezterm.target_triple, ".*pc-windows-msvc$") then
-  -- Configs for Windows only
-  -- font_dirs = {
-  --     'C:\\Users\\whoami\\.dotfiles\\.fonts'
-  -- }
-  -- local gitbash = {"C:\\Program Files\\Git\\bin\\bash.exe", "-i", "-l"}
+if string.match(wezterm.target_triple, '.*windows.*') ~= nil then
+  --- Grab the ver info for later use.
+  local success, stdout, stderr = wezterm.run_child_process { 'cmd.exe', 'ver' }
+  local major, minor, build, rev = stdout:match("Version ([0-9]+)%.([0-9]+)%.([0-9]+)%.([0-9]+)")
+  local is_windows_11 = tonumber(build) >= 22000
+  
+  --- Make it look cool.
+  if is_windows_11 then
+    wezterm.log_info "We're running Windows 11!"
+  end
+
   local pwsh = {"D:\\scoop\\apps\\pwsh\\current\\pwsh.exe"}
   local pwshbeta = {"D:\\scoop\\apps\\pwsh-beta\\current\\pwsh.exe"}
   local gitbash = {"D:\\scoop\\apps\\git\\current\\bin\\bash.exe", "-i", "-l"}
-  -- default_prog = {'wsl.exe', '~', '-d', 'Ubuntu-20.04'}
-  default_font = "Firge35Nerd Console" -- HackGen35Nerd Console, Firge35Nerd Console
+  local default_prog = {'wsl.exe', '~', '-d', 'Ubuntu-20.04'}
+  -- local default_font = "Firge35Nerd Console" -- HackGen35Nerd Console, Firge35Nerd Console
+  config.font = wezterm.font_with_fallback {
+  { family = 'Firge35Nerd Console' },
+  { family = 'HackGen35Nerd Console' },
+  { family = 'Hack Nerd Font', } }
+  config.default_prog = pwsh
+  config.launch_menu = {
+      { args = {"top"}, },
+      { label = "PowerShell", args = pwsh, },
+      { label = "pwsh-beta", args = pwshbeta, },
+      { label = "Git Bash", args = gitbash, }, }
 
-elseif string.match(wezterm.target_triple, ".*apple-darwin$") then
-  -- if wezterm.target_triple == 'x86_64-apple-darwin' then
+elseif string.match(wezterm.target_triple, '.*apple.*') ~= nil then
   -- Configs for OSX only
   -- font_dirs    = { '$HOME/.dotfiles/.fonts' }
-  default_font = "Monaco"
+  config.font = wezterm.font_with_fallback {
+  { family = 'Firge35Nerd Console' },
+  { family = 'HackGen35Nerd Console' },
+  { family = 'Monaco', } }
 
-elseif string.match(wezterm.target_triple, ".*linux-gnu$") then
+elseif string.match(wezterm.target_triple, '.*linux.*') ~= nil then
   -- if wezterm.target_triple == 'x86_65-unknown-linux-gnu' then
   -- Configs for Linux only
   -- font_dirs    = { '$HOME/.dotfiles/.fonts' }
-  default_font = "Monaco"
+  config.font = wezterm.font_with_fallback {
+  { family = 'Firge35Nerd Console' },
+  { family = 'HackGen35Nerd Console' },
+  { family = 'Monaco', } }
 
 else
-  default_font = "Monaco"
+  config.font = wezterm.font_with_fallback {
+  { family = 'Firge35Nerd Console' },
+  { family = 'HackGen35Nerd Console' },
+  { family = 'Monaco', } }
 end
 
 -- Title
@@ -56,103 +80,106 @@ wezterm.on( 'format-tab-title', function( tab, tabs, panes, config, hover, max_w
      } }
 end )
 
--- Startup
--- wezterm.on( 'gui-startup', function( cmd )
---     local tab, pane, window = wezterm.mux.spawn_window( cmd or {} )
---     window:gui_window():maximize()
--- end )
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+    local title = wezterm.truncate_right(utils.basename(tab.active_pane.foreground_process_name), max_width)
+    if title == "" then
+        title = wezterm.truncate_right(
+            utils.basename(utils.convert_home_dir(tab.active_pane.current_working_dir)),
+            max_width
+        )
+    end
+    return {
+        { Text = tab.tab_index + 1 .. ":" .. title },
+    }
+end)
 
-local config = {
-    -- Basic
-    check_for_updates = false,
-    switch_to_last_active_tab_when_closing_tab = false,
-    enable_scroll_bar = true,
+--- Default config settings 
+config.check_for_updates = false
+config.switch_to_last_active_tab_when_closing_tab = false
+config.enable_scroll_bar = true
 
-    -- window_background_image = "/path/to/wallpaper.jpg",
+-- window_background_image = "/path/to/wallpaper.jpg",
     
-    color_scheme = "Catppuccin Mocha", -- or Macchiato, Frappe, Latte, "Ollie", -- Batman, OneHalfDark
-    default_prog = pwsh,
-    font = wezterm.font(default_font),
-    font_size = 12.0,
-    use_ime = true, -- 
-
-    inactive_pane_hsb = {
+config.color_scheme = "Catppuccin Mocha" -- or Macchiato, Frappe, Latte, "Ollie", -- Batman, OneHalfDark
+config.font_size = 12.0
+config.use_ime = true
+config.inactive_pane_hsb = {
         hue = 1.0,
         saturation = 1.0,
         brightness = 1.0
-     },
+     }
     
-    -- Window
-    native_macos_fullscreen_mode = true,
-    adjust_window_size_when_changing_font_size = true,
-    window_background_opacity = 1,
-    window_padding = {
+-- Window
+config.native_macos_fullscreen_mode = true
+config.adjust_window_size_when_changing_font_size = true
+config.window_background_opacity = 1
+config.window_padding = {
         left = 5,
         right = 5,
         top = 5,
         bottom = 5
-     },
-    window_background_image_hsb = {
+     }
+config.window_background_image_hsb = {
         brightness = 0.8,
         hue = 1.0,
         saturation = 1.0
-     },
+     }
 
-    -- Tab Bar
-    enable_tab_bar = true,
-    adjust_window_size_when_changing_font_size = false,
-    -- disable_default_key_bindings = true,
-    enable_tab_bar = true,
-    hide_tab_bar_if_only_one_tab = false,
-    -- hide_tab_bar_if_only_one_tab = true,
-    show_tab_index_in_tab_bar = false,
-    tab_bar_at_bottom = true,
-    tab_max_width = 25,
+-- Tab Bar
+config.enable_tab_bar = true
+config.adjust_window_size_when_changing_font_size = false
+-- disable_default_key_bindings = true,
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = false
+-- hide_tab_bar_if_only_one_tab = true,
+config.show_tab_index_in_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 25
 
-    -- Keys
-    disable_default_key_bindings = false,
-    leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 },
-    keys = {
-        {key="l", mods='LEADER', action = wezterm.action.ShowLauncher } ,
+-- Keys
+config.disable_default_key_bindings = false
+config.leader = { key = 'b', mods = 'CTRL', timeout_milliseconds = 1000 }
+config.keys = {
+    {key="l", mods='LEADER', action = wezterm.action.ShowLauncher } ,
 
-        -- Activate the tab navigator UI in the current tab. 
-        -- The tab navigator displays a list of tabs and 
-        -- allows you to select and activate a tab from that list.
-        -- {key="F9", mods='LEADER', action="ShowTabNavigator"},
-        -- New/close pane
-        { key = 'c', mods = 'LEADER', action = wezterm.action { SpawnTab = 'CurrentPaneDomain' } }, 
-        { key = 'x', mods = 'LEADER', action = wezterm.action { CloseCurrentPane = { confirm = true } } }, 
-        { key = 'X', mods = 'LEADER', action = wezterm.action { CloseCurrentTab = { confirm = true } } },
-        -- Pane navigation
-        { key = 'LeftArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Left' } }, 
-        { key = 'DownArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Down' } }, 
-        { key = 'UpArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Up' } }, 
-        { key = 'RightArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Right' } }, 
-        -- Copy/paste buffer
-        -- Active Copy mode
-        { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
-        -- Copy
-        { key = 'y', mods = 'LEADER', action = wezterm.action { CopyTo = 'Clipboard' }}, 
-        -- Paste
-        { key = 'p', mods = 'LEADER', action = wezterm.action { PasteFrom = 'Clipboard' }}, 
-        -- { key = '[', mods = 'LEADER', action = 'ActivateCopyMode' }, 
-        { key = ']', mods = 'LEADER', action = 'QuickSelect' } ,
+    -- Activate the tab navigator UI in the current tab. 
+    -- The tab navigator displays a list of tabs and 
+    -- allows you to select and activate a tab from that list.
+    -- {key="F9", mods='LEADER', action="ShowTabNavigator"},
+    -- New/close pane
+    { key = 'c', mods = 'LEADER', action = wezterm.action { SpawnTab = 'CurrentPaneDomain' } }, 
+    { key = 'x', mods = 'LEADER', action = wezterm.action { CloseCurrentPane = { confirm = true } } }, 
+    { key = 'X', mods = 'LEADER', action = wezterm.action { CloseCurrentTab = { confirm = true } } },
+    -- Pane navigation
+    { key = 'LeftArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Left' } }, 
+    { key = 'DownArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Down' } }, 
+    { key = 'UpArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Up' } }, 
+    { key = 'RightArrow', mods = 'ALT', action = wezterm.action { ActivatePaneDirection = 'Right' } }, 
+    -- Copy/paste buffer
+    -- Active Copy mode
+    { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+    -- Copy
+    { key = 'y', mods = 'LEADER', action = wezterm.action { CopyTo = 'Clipboard' }}, 
+    -- Paste
+    { key = 'p', mods = 'LEADER', action = wezterm.action { PasteFrom = 'Clipboard' }}, 
+    -- { key = '[', mods = 'LEADER', action = 'ActivateCopyMode' }, 
+    { key = ']', mods = 'LEADER', action = 'QuickSelect' } ,
 
-        -- Tab
-        { key = 't', mods = 'LEADER', action = 'ShowTabNavigator' }, 
-        { key = 'b', mods = 'LEADER', action = 'ActivateLastTab' }, 
-        -- { key = '{', mods = 'ALT', action = act.ActivateTabRelative(-1) },
-        -- { key = '}', mods = 'ALT', action = act.ActivateTabRelative(1) },
-        { key = 'h', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = -1 } }, 
-        { key = 'l', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = 1 } },
-        { key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentTab { confirm = true }, },
+    -- Tab
+    { key = 't', mods = 'LEADER', action = 'ShowTabNavigator' }, 
+    { key = 'b', mods = 'LEADER', action = 'ActivateLastTab' }, 
+    -- { key = '{', mods = 'ALT', action = act.ActivateTabRelative(-1) },
+    -- { key = '}', mods = 'ALT', action = act.ActivateTabRelative(1) },
+    { key = 'h', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = -1 } }, 
+    { key = 'l', mods = 'CTRL', action = wezterm.action { ActivateTabRelative = 1 } },
+    { key = 'w', mods = 'CTRL', action = wezterm.action.CloseCurrentTab { confirm = true }, },
 
-        -- Split
-        { key = '-', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } }, 
-        { key = '\\', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }}, 
-    },
+    -- Split
+    { key = '-', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } }, 
+    { key = '\\', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' }}, 
+    }
 
-    mouse_bindings = {  -- Paste on right-click
+config.mouse_bindings = {  -- Paste on right-click
     {
         event = {
             Down = {
@@ -187,27 +214,13 @@ local config = {
          },
         mods = 'CTRL',
         action = 'OpenLinkAtMouseCursor'
-     } }, 
+     } } 
 
-    launch_menu = {
-        { args = {"top"}, },
-        { label = "PowerShell", args = pwsh, },
-        { label = "pwsh-beta", args = pwshbeta, },
-        { label = "Git Bash", args = gitbash, },
-    },
-    wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
-	local title = wezterm.truncate_right(utils.basename(tab.active_pane.foreground_process_name), max_width)
-	if title == "" then
-		title = wezterm.truncate_right(
-			utils.basename(utils.convert_home_dir(tab.active_pane.current_working_dir)),
-			max_width
-		)
-	end
-	return {
-		{ Text = tab.tab_index + 1 .. ":" .. title },
-	}
-    end),
-}
 
+
+-- Allow overwriting for work stuff
+if haswork then
+  work.apply_to_config(config)
+end
 return config
 
